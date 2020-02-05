2bb64b82f92283d565de9a82936c6574c2e3764d
Gwowen Fu <gwowen.fu@adtalem.com>
2020-02-05 11:51:40 -0600

    Update socket server name

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -2,7 +2,8 @@ import Vue from 'vue';
 import * as io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io-extended';
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
+const socket = io('ws://mobile-common-services-qa.herokuapp.com');
 
 export default () => {
   Vue.use(VueSocketIO, socket);
