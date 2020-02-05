30dae3bed02e92522bd82cc736591e0edf274a62
Gwowen Fu <gwowen.fu@adtalem.com>
2020-02-05 14:16:55 -0600

    Test

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,13 +1,10 @@
-import Vue from "vue";
-import * as io from "socket.io-client";
-import VueSocketIO from "vue-socket.io-extended";
+import Vue from 'vue';
+import * as io from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io-extended';
 
 // const socket = io('http://localhost:3000');
-const socket =
-  process.env.DEPLOY_ENV === "GH_PAGES"
-    ? io("wss://mobile-common-services-qa.herokuapp.com")
-    : io("ws://mobile-common-services-qa.herokuapp.com");
+const socket = io('wss://mobile-common-services-qa.herokuapp.com');
 
 export default () => {
   Vue.use(VueSocketIO, socket);
-};
+}
