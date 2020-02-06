9934fd5fcb27fc400d1f8abaa2209649b299dd3a
Gwowen Fu <gwowen.fu@adtalem.com>
2020-02-06 10:15:56 -0600

    Refactor

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -2,9 +2,7 @@ import Vue from 'vue';
 import * as io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io-extended';
 
-// const socket = io('http://localhost:3000');
-const socket = io('ws://mobile-common-services-qa.herokuapp.com');
-// const socket = io('wss://mobile-common-services-qa.herokuapp.com');
+const socket = io('wss://mobile-common-services-qa.herokuapp.com');
 
 export default () => {
   Vue.use(VueSocketIO, socket);
