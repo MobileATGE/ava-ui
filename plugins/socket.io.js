18207dffcf3206eb9bbdb5942fbea30c32cfacac
Gwowen Fu <gwowen.fu@adtalem.com>
2020-02-10 14:10:49 -0600

    Check dev environment

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -2,20 +2,20 @@ import Vue from "vue";
 import * as io from "socket.io-client";
 import VueSocketIO from "vue-socket.io-extended";
 
-const isProd = process.env.DEPLOY_ENV === "GH_PAGES";
+const isDev = process.env.DEPLOY_ENV === "DEV";
 let socket;
 
-console.log('isProd=' + isProd)
+console.log('isDev=' + isDev)
 
-if (isProd) {
-  socket = io("wss://mobile-common-services-qa.herokuapp.com", {
-    transports: ["websocket"]
-  })
-} else {
+if (isDev) {
   socket = io("ws://localhost:3000", {
     transports: ["websocket"]
   })
-}
+} else {
+  socket = io("wss://mobile-common-services-qa.herokuapp.com", {
+    transports: ["websocket"]
+  })
+} 
 
 export default () => {
   Vue.use(VueSocketIO, socket)
