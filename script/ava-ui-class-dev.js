4694eb59eb1450497dbd622b29e9ebdbad3b0999
Gwowen Fu <gwowen.fu@adtalem.com>
2020-06-08 10:59:58 -0500

    Reposition Ava icon with description

--- script/ava-ui-class-dev.js
+++ script/ava-ui-class-dev.js
@@ -52,17 +52,29 @@ class AvaUI {
     let url = 'https://ava-ui-dev.herokuapp.com/chat/';
     let params = '?id=' + user.login_id + '&name=' + user.name + '&email=' + user.primary_email + '&avatar=' + user.avatar;
     user.avaUrl = url + params;
-  
+
     // Build launch button
-    let g = document.createElement('button');
+    let g = document.createElement('a');
     g.innerHTML = '<img style="border-radius: 50%;" src="https://ava-ui-dev.herokuapp.com/ava-icon.png" class="sc-open-icon" width="30" height="30"/>';
     g.setAttribute("id", 'avaLauncher');
-    g.style.cssText = 'position:fixed;bottom:40px;right:15px;opacity:1;z-index:11;background-color:rgba(0,0,0,0);width:4em;height:4em;border-radius:50%;outline:none;';
-    document.body.appendChild(g);
+    g.setAttribute("href", "javascript:void(0)");
+    g.setAttribute("class", "ic-app-header__menu-list-link");
+    g.style.cssText = 'background-color:rgba(0,0,0,0);outline:none;';
+
+    let div = document.createElement('div');
+    div.setAttribute('class', 'menu-item-icon-container');
+    div.appendChild(g);
+    var newLi = document.createElement('li');
+    newLi.setAttribute('class', 'menu-item ic-app-header__menu-list-item');
+    newLi.appendChild(div);
+
+    let parent = document.querySelector('.ic-app-header__menu-list');
+    parent.insertBefore(newLi, document.querySelector('.ic-app-header__menu-list li:last-child'));
+
     document.querySelector('#avaLauncher').addEventListener('click', function () {
-      let left = screen.width - 620;
-      let params = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,width=600,height=680,top=0,left=' + left;
-      window.open(user.avaUrl, 'Ava', params);
+        let left = screen.width - 620;
+        let params = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=yes,width=600,height=680,top=0,left=' + left;
+        window.open(user.avaUrl, 'Ava', params);
     });
   }
 }
